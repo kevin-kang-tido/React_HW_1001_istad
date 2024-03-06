61eff439b8b460c9d86055a7d22b68de7b9322b6
kevin-kang-tido <kangdev78@gmail.com>
2024-03-06 21:41:02 +0700

    fix at app jsx pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,30 @@ function App() {
   return (
     <>
       <Navbars/>
-      <h2 className='text-lg md:text-2xl text-center font-bold m-6'>Products</h2>
-      {/* // Produt section  */}
-       <section className='container mx-auto p-5'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 items-center justify-center'>
-          <CardProduct/>
-          <CardProduct/>
-          <CardProduct/>
-          <CardProduct/>
-        </div>
-       </section>
-      <h2 className='text-lg md:text-2xl text-center font-bold m-6'>Feedback</h2>
-       <section className='container mx-auto p-5 m-6'>
-        <div className='grid grid-cols-1 grid-cols-3 md:grid-cols-4 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
-           <CardFeedback/>
-           <CardFeedback/>
-           <CardFeedback/>
-           <CardFeedback/>
-        </div>
-       </section>
+      <Main>
+          <h2 className='text-lg md:text-2xl text-center font-bold m-6'>Products</h2>
+          {/* // Produt section  */}
+          <section className='container mx-auto p-5'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 items-center justify-center'>
+              <CardProduct/>
+              <CardProduct/>
+              <CardProduct/>
+              <CardProduct/>
+            </div>
+          </section>
+          <h2 className='text-lg md:text-2xl text-center font-bold m-6'>Feedback</h2>
+          <section className='container mx-auto p-5 m-6'>
+            <div className='grid grid-cols-1 grid-cols-3 md:grid-cols-4 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
+              <CardFeedback/>
+              <CardFeedback/>
+              <CardFeedback/>
+              <CardFeedback/>
+            </div>
+          </section>
+      </Main>
+      
 
-      <Footers className='bottom-0'/>
+      <Footers/>
     </>
   )
 }
