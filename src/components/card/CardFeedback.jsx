168dc2e7049792a10d28224a154918567d59953d
kevin-kang-tido <kangdev78@gmail.com>
2024-03-06 21:32:47 +0700

    finish add commponent

---
+++ src/components/card/CardFeedback.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Card } from 'flowbite-react';
+
+export default function CardFeedback() {
+  return (
+    <>
+    <Card href="#" className="max-w-sm">
+      <h5 className="text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+      Luxury Watches and Jewelry Online Collections | Majordor®
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400 line-clamp-3">
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+      </p>
+    </Card>
+    </>
+  )
+}
---
+++ src/components/header/Navbar.jsx
@@ -0,0 +1,29 @@
+
+import React from 'react'
+import { Button, Navbar } from 'flowbite-react';
+
+export default function Navbars() {
+    return (
+        <>
+     <Navbar fluid rounded className='border top-0'>
+      <Navbar.Brand href="#">
+        <img src="https://i.pinimg.com/564x/4d/76/01/4d76019a06cd392cc8f854f225567383.jpg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">ISTAD REACT</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Button>Get started</Button>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="#" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="#">About</Navbar.Link>
+        <Navbar.Link href="#">Services</Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Contact</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+ </>
+    );
+}
